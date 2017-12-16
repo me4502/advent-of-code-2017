e62ebeb22c639307e4a880bc2681673d1c2d6140
Matthew Miller <mnmiller1@me.com>
2017-12-16 13:23:24 +1000

    Day 1

--- src/main/java/com/me4502/adventofcode/Day_1.java
+++ src/main/java/com/me4502/adventofcode/Day_1.java
@@ -3,6 +3,17 @@ package com.me4502.adventofcode;
 public class Day_1 {
 
     public static void main(String[] args) {
-        
+        String input = args[0];
+        int sum = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            char current = input.charAt(i);
+            char next = input.charAt((i + 1) % input.length());
+            if (current == next) {
+                sum += Character.getNumericValue(current);
+            }
+        }
+
+        System.out.println(sum);
     }
 }
